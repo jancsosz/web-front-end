e3b9e50063439d6379c4fe468af17fb6d9efac4d
Janos Geszteredi <geszteredi.janika98@gmail.com>
2021-12-12 04:04:43 +0100

    imported necessary modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,23 +10,56 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatIconModule} from '@angular/material/icon';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SignupComponent } from './components/signup/signup.component';
+import { SigninComponent } from './components/signin/signin.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {ReactiveFormsModule} from '@angular/forms';
+import { RegisterCarComponent } from './components/register-car/register-car.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import { MyCarsComponent } from './components/my-cars/my-cars.component';
+import { UpdateCarDialogComponent } from './components/dialog/update-car-dialog/update-car-dialog.component';
+import {MatDialog, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
-    CarsComponent
+    CarsComponent,
+    SignupComponent,
+    SigninComponent,
+    RegisterCarComponent,
+    MyCarsComponent,
+    UpdateCarDialogComponent
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatMenuModule,
     MatIconModule,
-    NgbModule
+    NgbModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatGridListModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatSortModule,
+    MatDialogModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MatDialogRef,
+      useValue: {}
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
