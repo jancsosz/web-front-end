7c43d7ab80ec44c204a8ec4a3df21cccea330ff2
Janos Geszteredi <geszteredi.janika98@gmail.com>
2021-12-12 04:01:32 +0100

    updated new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {CarsComponent} from './components/cars/cars.component';
+import {SigninComponent} from './components/signin/signin.component';
+import {SignupComponent} from './components/signup/signup.component';
+import {RegisterCarComponent} from './components/register-car/register-car.component';
+import {MyCarsComponent} from './components/my-cars/my-cars.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent, data: {title: 'Home'}},
   {path: 'cars', component: CarsComponent, data: {title: 'Cars'}},
+  {path: 'register-car', component: RegisterCarComponent},
+  {path: 'own-cars', component: MyCarsComponent},
+  {path: 'signin', component: SigninComponent},
+  {path: 'signup', component: SignupComponent},
   { path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
 
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -9,15 +9,26 @@ import {Router} from '@angular/router';
 export class MenuComponent implements OnInit {
   menu: any;
   innerWidth: number;
+  isLoggedIn: boolean;
+  username = '';
 
   constructor(
     private router: Router) { }
 
   ngOnInit(): void {
     this.innerWidth = window.innerWidth;
+    this.isLoggedIn = !!window.localStorage.getItem('username');
+    this.isLoggedIn ? this.username = window.localStorage.getItem('username') : this.username = '';
   }
 
   navigateTo(path: string): void {
     this.router.navigate([path]);
+  }
+
+  logOut(): void {
+    window.localStorage.removeItem('username');
+    window.localStorage.removeItem('token');
+    this.isLoggedIn = false;
+    this.navigateTo('home');
   }
 }
