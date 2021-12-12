808a082abc402094e4801d7a2b501c299d25deb8
Janos Geszteredi <geszteredi.janika98@gmail.com>
2021-12-12 04:04:04 +0100

    created cars component functionality

--- src/app/components/cars/cars.component.ts
+++ src/app/components/cars/cars.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {CarRegistryService} from '../../services/car-registry.service';
+import {CarDTO} from '../../models/dto/CarDTO';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-cars',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarsComponent implements OnInit {
 
-  constructor() { }
+  registeredCars: MatTableDataSource<CarDTO> = new MatTableDataSource<CarDTO>();
+
+  constructor(
+    private carRegistryService: CarRegistryService
+  ) { }
 
   ngOnInit(): void {
+    this.carRegistryService.getAllRegisteredCars(window.localStorage.getItem('token'))
+      .then(result => {
+        this.registeredCars = new MatTableDataSource(result);
+      });
   }
 
 }
